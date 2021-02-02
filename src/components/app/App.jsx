3a206505c15d37fc62a41e2dfafca9a9264004d2
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-02 14:20:44 -0800

    router page added

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainPage from '../MainPage/MainPage';
 
 export default class App extends Component {
   render() {
